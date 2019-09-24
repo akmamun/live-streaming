dcff23f3b536ac678385fa9ca0681182d315e40d
mrxmamun <almamunkhan04@gmail.com>
2019-09-24 14:28:14 +0600

    init react project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Javascript Camera Streaming 
+- Stream From WebCam or Mobile Camera
+- Take a Capture From Video
+
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"; 
+import Webcam from "./components/Webcam";
+
+export default function App() {
+  return (
+  //  <Webcam/>
+    );
+  }
